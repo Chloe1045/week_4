cffe683e208f06afd382a2a884d2125984bb7ee7
Chloe <lifesimmer1029@gmail.com>
2018-07-24 15:36:23 +0000

    added math

---
+++ var.py
@@ -0,0 +1,20 @@
+print("17")
+print 17
+print 17.23
+
+x = ("17")
+y= 17
+
+print x+str
+print int(x)+y
+
+y = y+1
+if x == y :
+    print("nay")
+elif x > y: 
+        print("bigger")      
+elif x > y: 
+            print("smaller")
+else: 
+                print("nay")
+            
